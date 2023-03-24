1a19553e8db9630314fb23eec14e4435c8d2afef
biofermin2 <supercaveman@gmail.com>
2023-03-25 02:22:53 +0900

    fix symbol

--- docs/js/header.js
+++ docs/js/header.js
@@ -1,6 +1,6 @@
 function header(){
     $.ajax({
-      url: "https://biofermin2.github.io/docs/header.html",
+      url: "htts://biofermin2.github.io/header.html",
         cache: false,
         async: false,
         dataType: 'html',
