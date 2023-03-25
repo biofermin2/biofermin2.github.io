4bc07c1829989e2acdfea734b0710f22172d037c
biofermin2 <supercaveman@gmail.com>
2023-03-25 09:08:58 +0900

    fix url

--- docs/js/header.js
+++ docs/js/header.js
@@ -1,6 +1,6 @@
 function header(){
     $.ajax({
-      url: "htts://biofermin2.github.io/header.html",
+      url: "https://biofermin2.github.io/header.html",
         cache: false,
         async: false,
         dataType: 'html',
